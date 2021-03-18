a175d3b6fe1d1f12505cd2cbc2aa53ebc61801b3
kohlkyle <kylekohl2@yahoo.com>
2021-03-17 21:34:46 -0400

    drop down sucks

--- src/app/vacation-form/vacation-form.component.ts
+++ src/app/vacation-form/vacation-form.component.ts
@@ -1,9 +1,9 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
-import { SygicService } from '../sygic.service';
-import { VacationService } from '../vacation.service';
-import { IDropdownSettings } from 'ng-multiselect-dropdown';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core'
+import { NgForm } from '@angular/forms'
+import { Router } from '@angular/router'
+import { SygicService } from '../sygic.service'
+import { VacationService } from '../vacation.service'
+import { IDropdownSettings } from 'ng-multiselect-dropdown'
 
 @Component({
   selector: 'app-vacation-form',
@@ -11,10 +11,12 @@ import { IDropdownSettings } from 'ng-multiselect-dropdown';
   styleUrls: ['./vacation-form.component.css'],
 })
 export class VacationFormComponent implements OnInit {
-  @Output() userForm: any = new EventEmitter<any>();
-  dropdownList = [];
-  selectedItems = [];
-  dropdownSettings = {};
+  @Output() userForm: any = new EventEmitter<any>()
+  @Output() selectTags: any = new EventEmitter<any>()
+  dropdownList: any = []
+  selectedItems: any = []
+  dropdownSettings: any = {}
+
   categories: string[] = [
     'Discovering',
     'eating',
@@ -27,7 +29,7 @@ export class VacationFormComponent implements OnInit {
     'Sleeping',
     'Doing Sports',
     'Traveling',
-  ];
+  ]
   noNoCategories: string[] = [
     'Discovering',
     'Eating',
@@ -40,9 +42,9 @@ export class VacationFormComponent implements OnInit {
     'Sleeping',
     'Doing Sports',
     'Traveling',
-  ];
-  lodging: string[] = ['Hotel', 'Campground', 'RV Park'];
-  position: any;
+  ]
+  lodging: string[] = ['Hotel', 'Campground', 'RV Park']
+  position: any
 
   constructor(
     private router: Router,
@@ -51,27 +53,46 @@ export class VacationFormComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // this.getResults()
-    // this.getSygicResults()
-    this.position = this.vacationService.getLocation();
-    if (!this.position) {
-      this.getAndSetLocation();
+    this.dropdownList = [
+      { item_id: 1, item_text: 'Mumbai' },
+      { item_id: 2, item_text: 'Bangaluru' },
+      { item_id: 3, item_text: 'Pune' },
+      { item_id: 4, item_text: 'Navsari' },
+      { item_id: 5, item_text: 'New Delhi' },
+    ]
+    this.selectedItems = [
+      { item_id: 3, item_text: 'Pune' },
+      { item_id: 4, item_text: 'Navsari' },
+    ]
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 3,
+      allowSearchFilter: true,
     }
-    this.dropdownList = [];
+
+    //
+    this.position = this.vacationService.getLocation()
+    if (!this.position) {
+      this.getAndSetLocation()
+    }
   }
 
   getAndSetLocation = (): any => {
     navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position.coords.latitude, position.coords.longitude);
-      this.position = position;
-      this.vacationService.setLocation(position);
-    });
-  };
+      console.log(position.coords.latitude, position.coords.longitude)
+      this.position = position
+      this.vacationService.setLocation(position)
+    })
+  }
 
   getFormData = (formData: NgForm): void => {
-    this.userForm.emit(formData);
-    console.log(formData);
-  };
+    this.userForm.emit(formData)
+    console.log(formData)
+  }
 
   //need to convert meters to miles
   // car : 45mph - 72,420 meters per hour -
@@ -82,20 +103,26 @@ export class VacationFormComponent implements OnInit {
   clean = (obj: any) => {
     for (let prop in obj) {
       if (!obj[prop]) {
-        delete obj[prop];
+        delete obj[prop]
       }
     }
-    console.log(obj);
-  };
+    console.log(obj)
+  }
 
   submitTripForm = (form: NgForm) => {
-    let obj: any = form.form.value;
-    this.clean(obj);
-    obj.lat = this.position.coords.latitude;
-    obj.lon = this.position.coords.longitude;
+    let obj: any = form.form.value
+    this.clean(obj)
+    obj.lat = this.position.coords.latitude
+    obj.lon = this.position.coords.longitude
 
     this.router.navigate(['/vacation-result'], {
       queryParams: obj,
-    });
-  };
+    })
+  }
+  onItemSelect(item: any) {
+    console.log(item)
+  }
+  onSelectAll(items: any) {
+    console.log(items)
+  }
 }
