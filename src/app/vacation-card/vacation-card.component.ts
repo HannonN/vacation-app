8839ad0955d74881edfe0d0a0bb8978d911c133d
nathan <natedrab@gmail.com>
2021-03-18 14:26:13 -0400

    before mvp

--- src/app/vacation-card/vacation-card.component.ts
+++ src/app/vacation-card/vacation-card.component.ts
@@ -7,11 +7,6 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class VacationCardComponent implements OnInit {
   @Input() tripRef: any; //should this be an array?
-  trips: any;
-  name: any;
-  name_suffix: any;
-  thumbnail_url: any;
-  details: any;
 
   constructor() {}
 
--- src/app/vacation-result/vacation-result.component.ts
+++ src/app/vacation-result/vacation-result.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
-import { SygicService } from '../sygic.service'
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SygicService } from '../sygic.service';
 
 @Component({
   selector: 'app-vacation-result',
@@ -8,6 +8,7 @@ import { SygicService } from '../sygic.service'
   styleUrls: ['./vacation-result.component.css'],
 })
 export class VacationResultComponent implements OnInit {
+  trips: any;
   constructor(
     private route: ActivatedRoute,
     private sygicService: SygicService
@@ -15,11 +16,16 @@ export class VacationResultComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.queryParamMap.subscribe((response: any) => {
-      console.log(response.params)
+      console.log(response.params);
 
-      this.sygicService.getItemsFromSygic(response.params).subscribe((data) => {
-        console.log(data)
-      })
-    })
+      this.getVacationCards(response.params);
+    });
   }
+
+  getVacationCards = (obj: any) => {
+    this.sygicService.getItemsFromSygic(obj).subscribe((response: any) => {
+      console.log(response.data.places);
+      this.trips = response.data.places;
+    });
+  };
 }
