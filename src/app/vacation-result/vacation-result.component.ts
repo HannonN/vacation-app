88119214f255084fb875659ba2859416b9026aeb
kohlkyle <kylekohl2@yahoo.com>
2021-03-25 14:46:09 -0400

    loading screens

--- src/app/vacation-result/vacation-result.component.ts
+++ src/app/vacation-result/vacation-result.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { SygicService } from '../sygic.service';
-import { VacationService } from '../vacation.service';
+import { Component, Input, OnInit } from '@angular/core'
+import { ActivatedRoute, Router } from '@angular/router'
+import { SygicService } from '../sygic.service'
+import { VacationService } from '../vacation.service'
 
 @Component({
   selector: 'app-vacation-result',
@@ -9,9 +9,9 @@ import { VacationService } from '../vacation.service';
   styleUrls: ['./vacation-result.component.css'],
 })
 export class VacationResultComponent implements OnInit {
-  trips: any;
-  position: any;
-  sortByResult: string = '';
+  trips: any
+  position: any
+  sortByResult: string = ''
   constructor(
     private route: ActivatedRoute,
     private router: Router,
@@ -21,86 +21,86 @@ export class VacationResultComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.queryParamMap.subscribe((response: any) => {
-      console.log(response.params);
+      console.log(response.params)
 
-      this.getVacationCards(response.params);
-    });
+      this.getVacationCards(response.params)
+    })
     //-----------------------------------------
-    this.position = this.vacationService.getLocation();
+    this.position = this.vacationService.getLocation()
     if (!this.position) {
-      this.vacationService.setLocation();
+      this.vacationService.setLocation()
     }
     this.route.queryParamMap.subscribe((response: any) => {
-      let destinationLocationObj: any = response.params;
-      console.log(destinationLocationObj);
+      let destinationLocationObj: any = response.params
+      console.log(destinationLocationObj)
 
       this.sygicService
         .getItemsFromSygic(response.params)
         .subscribe((response: any) => {
-          this.position = this.vacationService.getLocation();
+          this.position = this.vacationService.getLocation()
 
-          this.trips = response.data.places;
-          this.sygicService.fisherShuffle(this.trips);
+          this.trips = response.data.places
+          this.sygicService.fisherShuffle(this.trips)
           this.trips.forEach((item: any) => {
             item.distance = this.sygicService.getDistanceFromLatLonInMiles(
               this.position.coords.latitude,
               this.position.coords.longitude,
               item.location.lat,
               item.location.lng
-            );
-          });
+            )
+          })
           this.trips = this.trips.filter((item: any) => {
-            return item.perex && item.thumbnail_url;
-          });
-        });
-    });
+            return item.perex && item.thumbnail_url
+          })
+        })
+    })
   }
 
   getVacationCards = (obj: any) => {
     this.sygicService.getItemsFromSygic(obj).subscribe((response: any) => {
-      console.log(response.data.places);
-      this.trips = response.data.places;
-    });
-  };
+      console.log(response.data.places)
+      this.trips = response.data.places
+    })
+  }
 
   sortByDistanceResults = (sortBy: string) => {
-    let newArray: any = [];
+    let newArray: any = []
     if (sortBy === '') {
-      console.log('no sort');
-      return this.trips;
+      console.log('no sort')
+      return this.trips
     } else if (sortBy === 'ascending') {
-      console.log('ascending');
+      console.log('ascending')
 
       newArray = this.trips.sort((firstEl: any, secondEl: any) => {
         if (firstEl.distance < secondEl.distance) {
-          return -1;
+          return -1
         } else if (firstEl.distance > secondEl.distance) {
-          return 1;
+          return 1
         } else {
-          return 0;
+          return 0
         }
-      });
-      console.log(newArray);
+      })
+      console.log(newArray)
 
-      return newArray;
+      return newArray
     } else if (sortBy === 'descending') {
-      console.log('descending');
+      console.log('descending')
       newArray = this.trips.sort((firstEl: any, secondEl: any) => {
         if (firstEl.distance > secondEl.distance) {
-          return -1;
+          return -1
         } else if (firstEl.distance < secondEl.distance) {
-          return 1;
+          return 1
         } else {
-          return 0;
+          return 0
         }
-      });
-      console.log(newArray);
-      return newArray;
+      })
+      console.log(newArray)
+      return newArray
     }
-    console.log(newArray);
-  };
+    console.log(newArray)
+  }
 
   setSortResults = (sortTerm: string) => {
-    this.sortByResult = sortTerm;
-  };
+    this.sortByResult = sortTerm
+  }
 }
